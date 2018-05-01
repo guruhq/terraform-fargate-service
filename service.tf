1ed8413670deb1e53b34f58602cb6e080305907d
Karl Girthofer <karlgirthofer@gmail.com>
2018-05-01 11:37:48 -0400

    initial checkin including fargate adjustments

---
+++ service.tf
@@ -0,0 +1,23 @@
+resource "aws_ecs_service" "main" {
+  name            = "${var.project}"
+  cluster         = "${var.cluster_name}"
+  task_definition = "${var.task_definition}"
+  desired_count   = "${var.service_desired_count}"
+#  iam_role        = "arn:aws:iam::495243515911:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = "${var.private_subnet_ids}"
+    security_groups  = "${var.container_security_groups}"
+    assign_public_ip = "${var.assign_public_ip}"
+  }
+
+ load_balancer {
+    target_group_arn = "${var.target_group_arn}"
+    container_name   = "${var.lb_container_name}"
+    container_port   = "${var.lb_container_port}"
+  }
+
+  lifecycle {
+    ignore_changes = ["desired_count"]
+  }
+}
