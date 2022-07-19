685eec030343adba2a7df61caa880974abf370c7
Shawn Catanzarite <me@shawncatz.com>
2022-07-19 14:11:25 -0700

    remove string interpolation and add ignore changes for balancer

--- service.tf
+++ service.tf
@@ -1,28 +1,28 @@
 resource "aws_ecs_service" "main" {
-  name            = "${var.project}"
-  cluster         = "${var.cluster_name}"
-  task_definition = "${var.task_definition}"
-  desired_count   = "${var.service_desired_count}"
+  name            = var.project
+  cluster         = var.cluster_name
+  task_definition = var.task_definition
+  desired_count   = var.service_desired_count
   launch_type     = "FARGATE"
   network_configuration {
-    subnets          = "${var.private_subnet_ids}"
-    security_groups  = "${var.container_security_groups}"
-    assign_public_ip = "${var.assign_public_ip}"
+    subnets          = var.private_subnet_ids
+    security_groups  = var.container_security_groups
+    assign_public_ip = var.assign_public_ip
   }
 
- load_balancer {
-    target_group_arn = "${var.target_group_arn}"
-    container_name   = "${var.lb_container_name}"
-    container_port   = "${var.lb_container_port}"
+  load_balancer {
+    target_group_arn = var.target_group_arn
+    container_name   = var.lb_container_name
+    container_port   = var.lb_container_port
   }
 
- health_check_grace_period_seconds = "${var.health_check_grace_period_seconds}"
+  health_check_grace_period_seconds = var.health_check_grace_period_seconds
 
   lifecycle {
-    ignore_changes = ["desired_count"]
+    ignore_changes = [load_balancer, desired_count]
   }
 
   deployment_controller {
-    type = "${var.deployment_type}"
+    type = var.deployment_type
   }
 }
