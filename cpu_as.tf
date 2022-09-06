f8ee89ce11e711862426cca892be6d976a6df5bc
Bob Ziuchkovski <bziuchkovski@getguru.com>
2022-09-06 08:32:02 -0600

    terraform fmt

--- cpu_as.tf
+++ cpu_as.tf
@@ -1,18 +1,18 @@
 resource "aws_cloudwatch_metric_alarm" "service_cpu_high" {
   alarm_name          = "${var.project}-${var.environment}-service-CPU-High"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "${var.cpu_up_evaluation_periods}"
+  evaluation_periods  = var.cpu_up_evaluation_periods
   metric_name         = "CPUUtilization"
   namespace           = "AWS/ECS"
-  period              = "${var.cpu_up_time_period}"
+  period              = var.cpu_up_time_period
   statistic           = "Average"
-  threshold           = "${var.cpu_threshold_up}"
+  threshold           = var.cpu_threshold_up
 
   dimensions = {
     ClusterName = "${var.cluster_name}"
     ServiceName = "${aws_ecs_service.main.name}"
   }
-  
+
   alarm_actions = [
     "${aws_appautoscaling_policy.cpu_up.arn}"
   ]
@@ -21,12 +21,12 @@ resource "aws_cloudwatch_metric_alarm" "service_cpu_high" {
 resource "aws_cloudwatch_metric_alarm" "service_cpu_low" {
   alarm_name          = "${var.project}-${var.environment}-service-CPU-Low"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "${var.cpu_down_evaluation_periods}"
+  evaluation_periods  = var.cpu_down_evaluation_periods
   metric_name         = "CPUUtilization"
   namespace           = "AWS/ECS"
-  period              = "${var.cpu_down_time_period}"
+  period              = var.cpu_down_time_period
   statistic           = "Average"
-  threshold           = "${var.cpu_threshold_down}"
+  threshold           = var.cpu_threshold_down
 
   dimensions = {
     ClusterName = "${var.cluster_name}"
@@ -39,12 +39,12 @@ resource "aws_cloudwatch_metric_alarm" "service_cpu_low" {
 }
 
 resource "aws_appautoscaling_target" "scale_target" {
-  service_namespace = "ecs"
-  resource_id = "service/${var.cluster_name}/${aws_ecs_service.main.name}"
+  service_namespace  = "ecs"
+  resource_id        = "service/${var.cluster_name}/${aws_ecs_service.main.name}"
   scalable_dimension = "ecs:service:DesiredCount"
-  role_arn = "${var.service_role_arn}"
-  min_capacity = "${var.service_asg_min_cap}"
-  max_capacity = "${var.service_asg_max_cap}"
+  role_arn           = var.service_role_arn
+  min_capacity       = var.service_asg_min_cap
+  max_capacity       = var.service_asg_max_cap
 
   depends_on = [
     "aws_ecs_service.main"
@@ -53,14 +53,14 @@ resource "aws_appautoscaling_target" "scale_target" {
 }
 
 resource "aws_appautoscaling_policy" "cpu_up" {
-  name                      = "${var.project}-${var.environment}-scale-up"
-  service_namespace         = "ecs"
-  resource_id               = "service/${var.cluster_name}/${aws_ecs_service.main.name}"
-  scalable_dimension        = "ecs:service:DesiredCount"
+  name               = "${var.project}-${var.environment}-scale-up"
+  service_namespace  = "ecs"
+  resource_id        = "service/${var.cluster_name}/${aws_ecs_service.main.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
   step_scaling_policy_configuration {
-    adjustment_type           = "ChangeInCapacity"
-    cooldown                  = "${var.cpu_up_cooldown}"
-    metric_aggregation_type   = "Average"
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = var.cpu_up_cooldown
+    metric_aggregation_type = "Average"
     step_adjustment {
       metric_interval_lower_bound = 0
       scaling_adjustment          = 1
@@ -72,18 +72,18 @@ resource "aws_appautoscaling_policy" "cpu_up" {
 }
 
 resource "aws_appautoscaling_policy" "cpu_down" {
-  name                      = "${var.project}-${var.environment}-scale-down"
-  service_namespace         = "ecs"
-  resource_id               = "service/${var.cluster_name}/${aws_ecs_service.main.name}"
-  scalable_dimension        = "ecs:service:DesiredCount"
+  name               = "${var.project}-${var.environment}-scale-down"
+  service_namespace  = "ecs"
+  resource_id        = "service/${var.cluster_name}/${aws_ecs_service.main.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
   step_scaling_policy_configuration {
-    adjustment_type           = "ChangeInCapacity"
-    cooldown                  = "${var.cpu_down_cooldown}" 
-    metric_aggregation_type   = "Average"
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = var.cpu_down_cooldown
+    metric_aggregation_type = "Average"
 
     step_adjustment {
       metric_interval_upper_bound = 0
-      scaling_adjustment = -1
+      scaling_adjustment          = -1
     }
   }
   depends_on = [
